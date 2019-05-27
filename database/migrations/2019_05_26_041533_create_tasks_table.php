b1a7499004f6b3b96fd5efaa0de831a0943bd41b
Nikolina <nikolina.cesic@hotmail.com>
2019-05-27 11:58:00 +0200

    LV5

---
+++ database/migrations/2019_05_26_041533_create_tasks_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTasksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->increments('id');
+
+            $table->integer('user_id')->unsigned();
+            $table->foreign('user_id')->references('id')->on('users');
+
+            $table->string('naziv_rada');
+            $table->string('naziv_rada_na_eng');
+            $table->string('zadatak_rada');
+            $table->string('tip_studija');
+            $table->timestamps();
+        });
+
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('tasks');
+    }
+}
